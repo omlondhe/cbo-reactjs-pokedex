6175bd61392a00a3aac8f16c64d1db6ba346db07
Om Londhe <oplondhe@gmail.com>
2025-02-22 17:29:02 +0530

    FIX: Image carousel count and pagination

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -18,17 +18,17 @@ const Pokemon = () => {
 	const image = images[selectedImageIndex];
 
 	return (
-		<section className="flex gap-5 relative">
+		<section className="flex gap-5">
 			{image && (
 				<img
 					src={image.image}
 					alt={image.imageType}
-					className="w-screen h-full object-cover absolute top-0 left-0 scale-200 opacity-20 -z-10 blur-md"
+					className="max-w-screen w-full h-full object-cover absolute top-0 left-0 opacity-30 scale-150 -z-10 blur-2xl"
 				/>
 			)}
 			<header
 				className={clsx(
-					"flex items-center max-w-screen gap-5 w-full h-96",
+					"flex items-center max-w-screen gap-5 w-full",
 					isLoading &&
 						"bg-gray-100 animate-pulse border border-gray-300"
 				)}
@@ -49,8 +49,8 @@ const Pokemon = () => {
 							<ArrowBackIosNewRoundedIcon className="h-4 w-4 text-gray-900" />
 							&nbsp;
 						</Button>
-						<div className="flex flex-col relative h-96 gap-7 min-w-96 p-5 border bg-white/20 border-white/75 rounded-md flex-1">
-							<div className="flex-1 h-80">
+						<div className="flex flex-col relative min-w-96 gap-2.5 p-5 border bg-white/20 border-white/75 rounded-md flex-1">
+							<div className="h-96">
 								<img
 									src={image.image}
 									alt={image.imageType}
@@ -74,6 +74,38 @@ const Pokemon = () => {
 									</>
 								)}
 							</div>
+							{images.length && (
+								<div className="flex items-center gap-2 justify-center w-fit mx-auto">
+									<div className="flex items-center gap-2 h-7 bg-white py-2 px-4 rounded-full">
+										{Array(images.length)
+											.fill(1)
+											.map((_, index) => (
+												<button
+													key={index}
+													onClick={() =>
+														setSelectedImageIndex(
+															index
+														)
+													}
+													className={clsx(
+														"rounded-full transition-all duration-300 cursor-pointer",
+														index ===
+															selectedImageIndex
+															? "bg-black h-3 w-3"
+															: "bg-gray-400 h-2 w-2"
+													)}
+												/>
+											))}
+									</div>
+									<div className="py-2 px-4 h-7 bg-white rounded-full w-fit grid place-items-center">
+										<p className="text-sm font-medium -mt-1">
+											{`${selectedImageIndex + 1} of ${
+												images.length
+											}`}
+										</p>
+									</div>
+								</div>
+							)}
 						</div>
 						<Button
 							fullHeight={true}
